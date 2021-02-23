52c0c9aae60fbbba32ae304244e7f389a8d6aa5a
AdrianPlesner <adrian.plesner@gmail.com>
2021-02-23 10:06:00 +0100

    Import h5 to gluon and plot train and test data now functions

--- h5_to_gluonts.py
+++ h5_to_gluonts.py
@@ -1,45 +1,61 @@
 import pandas as pd
+
 from gluonts.dataset.common import ListDataset
 from gluonts.dataset.util import to_pandas
 import matplotlib.pyplot as plt
 
-store = pd.HDFStore('./pems-bay.h5')
-data = store['/speed']
-store.close()
-data_points = 288 * 31
-test_points = data_points + 288
-sensor = 0
-freq = '5Min'
-data_start = [pd.Timestamp("01-01-2019", freq=freq) for _ in range(test_points)]
 
-train_data = ListDataset([{
-    "start": start,
-    "target": target}
-    for (start, target) in zip(data_start[data_points:], [data.values[:data_points, sensor]])],
-    freq=freq
-)
-test_data = ListDataset([{
-    "start": start,
-    "target": target}
-    for (start, target) in zip(data_start[:test_points], [data.values[:test_points, sensor]])],
-    freq=freq
-)
-train_entry = next(iter(train_data))
+def load_h5_to_gluon(path, train_size=0, test_size=0, freq="1H"):
+    store = pd.HDFStore(path)
+    data = store[store.keys()[0]]
+    store.close()
+    sensor = 0
+    size = len(data.values)
+    if train_size == 0 and test_size == 0:
+        train_size = size - size // 10
+        test_size = size - train_size
+    elif train_size == 0:
+        train_size = size - test_size
+    elif test_size == 0:
+        test_size = size - train_size
+    data_points = train_size
+    test_points = test_size + train_size
 
-test_entry = next(iter(test_data))
+    data_start = [pd.Timestamp(data.axes[0].array[0], freq=freq) for _ in range(test_points)]
 
-train_series = to_pandas(train_entry)
-test_series = to_pandas(test_entry)
+    train_data = ListDataset([{
+        "start": start,
+        "target": target}
+        for (start, target) in zip(data_start[data_points:], [data.values[:data_points, sensor]])],
+        freq=freq
+    )
+    test_data = ListDataset([{
+        "start": start,
+        "target": target}
+        for (start, target) in zip(data_start[:test_points], [data.values[:test_points, sensor]])],
+        freq=freq
+    )
+    return train_data, test_data
 
-fig, ax = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(20, 10))
 
-train_series.plot(ax=ax[0])
-ax[0].grid(which="both")
-ax[0].legend(["train series"], loc="upper left")
+def plot_train_test(train_data, test_data, size_x=20, size_y=10):
+    train_entry = next(iter(train_data))
 
-test_series.plot(ax=ax[1])
-ax[1].axvline(train_series.index[-1], color='r')  # end of train dataset
-ax[1].grid(which="both")
-ax[1].legend(["test series", "end of train series"], loc="upper left")
+    test_entry = next(iter(test_data))
 
-plt.show()
+    train_series = to_pandas(train_entry)
+    test_series = to_pandas(test_entry)
+
+    fig, ax = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(size_x, size_y))
+
+    train_series.plot(ax=ax[0])
+    ax[0].grid(which="both")
+    ax[0].legend(["train series"], loc="upper left")
+
+    test_series.plot(ax=ax[1])
+    ax[1].axvline(train_series.index[-1], color='r')  # end of train dataset
+    ax[1].grid(which="both")
+    ax[1].legend(["test series", "end of train series"], loc="upper left")
+
+    plt.show()
+    return
--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import h5_to_gluonts as hg
+train, test = hg.load_h5_to_gluon("./data/pems-bay.h5", train_size=288*31, test_size=288, freq="5Min")
+hg.plot_train_test(train, test)
