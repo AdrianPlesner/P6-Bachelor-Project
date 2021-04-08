705cd516df093e0f2c932d642484c2795aaadcd7
AdrianPlesner <adrian.plesner@gmail.com>
2021-04-08 13:32:51 +0200

    Multiprocessing on validation

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,6 +2,8 @@ import numpy as np
 import properscoring as ps
 import pandas as pd
 from gluonts.dataset.common import ListDataset
+import multiprocessing as mp
+import time
 
 
 def evaluate_forecast(data, forecast, length):
@@ -16,6 +18,7 @@ def split_validation(data, md):
     step = md['prediction_length']
     t = pd.date_range(start=data.list_data[0]['start'], freq=md['freq'], periods=len(data.list_data[0]['target']))
     return [ListDataset([{
+        'start': t[n],
         'target': d['target'][n:n + step],
         'sensor_id': d['sensor_id'][n:n + step],
         'time_feat': d['time_feat'][::, n:n + step],
@@ -24,16 +27,60 @@ def split_validation(data, md):
 
 
 def validate(data, forecast):
+    print(f'Beginning data with start {data.list_data[0]["start"]}')
+    start = time.perf_counter()
     x = [np.sort(n.samples, 0) for n in forecast]
     evaluation = []
     for n in range(len(x)):
         ar = x[n].swapaxes(0, 1)
         cdf = [CdfShell(a) for a in ar]
-        evaluation.append(ps.crps_quadrature(data.list_data[n]['target'], cdf))
+        xmin = [-np.inf for _ in ar]
+        xmax = [np.inf for _ in ar]
+        b = ps.crps_quadrature(data.list_data[n]['target'], cdf, xmin, xmax)
+        evaluation.append(b)
+    end = time.perf_counter() - start
+    print(f'Data with start {data.list_data[0]["start"]} done. Time = {end}')
     return np.asarray(evaluation)
 
 
 validate_vector = np.vectorize(validate, otypes=[list])
+
+
+def validate_mp(data, forecast):
+    assert len(data) == len(forecast)
+    n_proc = mp.cpu_count()
+    chunk_size = len(data) // n_proc
+    rem = len(data) % n_proc
+    proc_chunks = []
+    # split in chunks for parallelization
+    for i_proc in range(n_proc):
+        chunkstart = i_proc * chunk_size
+        if i_proc < rem:
+            chunkstart += i_proc
+        else:
+            chunkstart += rem
+        # make sure to include the division remainder for the last process
+        chunkend = (i_proc + 1) * chunk_size
+        if i_proc < rem:
+            chunkend += i_proc + 1
+        else:
+            chunkend += rem
+
+        proc_chunks.append((data[chunkstart:chunkend], forecast[chunkstart:chunkend]))
+
+    assert sum([len(x[0]) for x in proc_chunks]) == len(data)
+
+    with mp.Pool(processes=n_proc) as pool:
+        # starts the sub-processes without blocking
+        # pass the chunk to each worker process
+        proc_results = [pool.apply_async(validate_vector, args=(chunk[0], chunk[1], )) for chunk in proc_chunks]
+
+        # blocks until all results are fetched
+        result_chunks = [r.get() for r in proc_results]
+    result = np.array([])
+    for n in result_chunks:
+        result = np.append(result, n)
+    return result
 
 
 class CdfShell:
--- src/main.py
+++ src/main.py
@@ -9,78 +9,73 @@ import numpy as np
 import sys
 import json
 import os
+import time
 
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        path = input("Missing program argument: metadata path\n"
+                     "please give it:")
+        if path == "":
+            exit()
+    else:
+        path = sys.argv[1]
 
-if len(sys.argv) < 2:
-    path = input("Missing program argument: metadata path\n"
-                 "please give it:")
-    if path == "":
-        exit()
-else:
-    path = sys.argv[1]
+    with open(path) as md_file:
+        md = json.load(md_file)
 
-with open(path) as md_file:
-    md = json.load(md_file)
+    ### Load data
+    start = time.perf_counter()
+    train, valid, test = hg.load_h5_to_gluon(md['path'], md)
+    # if md['make_plots']:
+    # hg.plot_train_test(train, test)
+    if md['normalize']:
+        for data in (train, valid, test):
+            for i in range(len(data)):
+                data.list_data[i]['target'], data.list_data[i]['scaler'] = dp.preprocess_data(
+                    data.list_data[i]['target'])
+    end = time.perf_counter() - start
+    print("Loading data took", end, "seconds")
+    # if md['make_plots']:
+    # hg.plot_train_test(train, test)
 
-### Load data
+    ### Train network
+    if md['train_predictor']:
+        start = time.perf_counter()
+        predictor = fc.train_predictor(train, md)
+        end = time.perf_counter() - start
+        print("Training the predictor took", end, "seconds")
+        if not os.path.isdir(md['serialize_path']):
+            os.makedirs(md['serialize_path'])
+        predictor.serialize(Path(md['serialize_path'] + "/"))
+    else:
+        ### Load pre-trained predictors
+        predictor = fc.load_predictor(md['serialize_path'])
 
-train, valid, test = hg.load_h5_to_gluon(md['path'], md)
-#if md['make_plots']:
-    #hg.plot_train_test(train, test)
-if md['normalize']:
-    for data in (train, valid, test):
-        for i in range(len(data)):
-            data.list_data[i]['target'], data.list_data[i]['scaler'] = dp.preprocess_data(data.list_data[i]['target'])
+    ### Compute validation metrics
+    validation_slices = evaluation.split_validation(valid, md)
+#    validation_slices = validation_slices
+    start = time.perf_counter()
+    forecast = fc.make_forecast_vector(predictor, validation_slices)
+    end = time.perf_counter() - start
+    print("Creating forecasts took", end, "seconds")
+    start = time.perf_counter()
+    validation_slices, forecast = dp.postprocess_data_vector(validation_slices, forecast)
+    end = time.perf_counter() - start
+    print("Rescaling took", end, "seconds")
+    start = time.perf_counter()
+    evals = evaluation.validate_mp(validation_slices, forecast)
+    end = time.perf_counter() - start
+    print("Evaluation took", end, "seconds")
 
-#if md['make_plots']:
-    #hg.plot_train_test(train, test)
+    with open(md['serialize_path'] + "evaluation.txt", "w") as file:
+        e = np.stack(evals)
+        for i in range(len(evals)):
+            file.write("Slice: " + str(i) + "\n")
+            for j in range(len(evals[i])):
+                file.write("Sensor: " + str(j) + "\n")
+                for k in range(len(evals[i][j])):
+                    file.write(str(evals[i][j][k]) + ", ")
+                file.write("\n")
 
-### Train network
-if md['train_predictor']:
-    predictor = fc.train_predictor(train, md)
-    if not os.path.isdir(md['serialize_path']):
-        os.makedirs(md['serialize_path'])
-    predictor.serialize(Path(md['serialize_path'] + "/"))
-else:
-    ### Load pre-trained predictors
-    predictor = fc.load_predictor(md['serialize_path'])
-
-# ### Compute validation metrics
-# validation_slices = evaluation.split_validation(valid, md)
-# validation_slices = validation_slices
-# forecast = fc.make_forecast_vector(predictor, validation_slices)
-# validation_slices, forecast = dp.postprocess_data_vector(validation_slices, forecast)
-# evals = evaluation.validate_vector(validation_slices, forecast)
-#
-#
-# with open(md['serialize_path']+"evaluation.txt", "w") as file:
-#     e = np.stack(evals)
-#     for i in range(len(evals)):
-#         file.write("Slice: " + str(i) + "\n")
-#         for j in range(len(evals[i])):
-#             file.write("Sensor: " + str(j) + "\n")
-#             for k in range(len(evals[i][j])):
-#                 file.write(str(evals[i][j][k]) + ", ")
-#             file.write("\n")
-#
-#         file.write("\n\n")
-#     file.write("Average: " + str(np.average(e)))
-
-
-# ### Make forecasts
-# forecast = fc.make_forecast(predictor, data, md)
-# if md['normalize']:
-#     for n in range(iterations):
-#         data[n], forecast[n] = dp.postprocess_data(data[n], forecast[n])
-#
-# ### Plot forecasts
-# if md['make_plots']:
-#     for n in range(iterations):
-#         fc.plot_forecast(data[n]['test'], forecast[n], n, md)
-#
-# ### Evaluate predictions
-# evals = []
-# for n in range(iterations):
-#     e = evaluation.evaluate_forecast(data[n]['test'], forecast[n], md['test_length'])
-#     evals.append(e)
-# print(evals)
+            file.write("\n\n")
+        file.write("Average: " + str(np.average(e)))
