0df3f1fda4a1ab77287e1d85ed68539a00d3394e
AdrianPlesner <adrian.plesner@gmail.com>
2021-05-03 15:00:34 +0200

    make scripts

--- src/main.py
+++ src/main.py
@@ -18,17 +18,19 @@ if __name__ == '__main__':
                      "please give it:")
         if path == "":
             exit()
-        # param = 'stop'
+        param = 'stop'
+        i = 0
     else:
         path = sys.argv[1]
-        # param = 'stop'
-        #
-        # if len(sys.argv) > 2:
-        #     param = sys.argv[2]
-        #     step = int(sys.argv[3])
-        #     div = int(sys.argv[4])
-        #     if div > 0:
-        #         step = step / div
+        param = 'stop'
+        i = 0
+        if len(sys.argv) > 2:
+            param = sys.argv[2]
+            i = sys.argv[3]
+            # step = int(sys.argv[3])
+            # div = int(sys.argv[4])
+            # if div > 0:
+            #     step = step / div
 
     with open(path) as md_file:
         md = json.load(md_file)
@@ -48,73 +50,72 @@ if __name__ == '__main__':
     # if md['make_plots']:
     # hg.plot_train_test(train, test)
     if 'params' in md:
-        for i in range(len(md['params'])):
-            param = md['params'][i]
-            md[param] = md['start'][i]
-            step = md['step'][i]
-            end = md['end'][i]
-            best = 10.0
-            result = 0
-            for p in range(md[param], end, step):
-                md[param] = p
-                try:
-                    ### Train network
-                    if md['train_predictor']:
-                        start = time.perf_counter()
-                        predictor = fc.train_predictor(train, md)
-                        end = time.perf_counter() - start
-                        print("Training the predictor took", end, "seconds")
-                        if not os.path.isdir(md['serialize_path']):
-                            os.makedirs(md['serialize_path'])
-                        predictor.serialize(Path(md['serialize_path'] + "/"))
-                    else:
-                        ### Load pre-trained predictors
-                        predictor = fc.load_predictor(md['serialize_path'], md)
+        param = md['params'][i]
+        md[param] = md['start'][i]
+        step = md['step'][i]
+        end = md['end'][i]
+        best = 10.0
+        result = 0
+        for p in range(md[param], end, step):
+            md[param] = p
+            try:
+                ### Train network
+                if md['train_predictor']:
+                    start = time.perf_counter()
+                    predictor = fc.train_predictor(train, md)
+                    end = time.perf_counter() - start
+                    print("Training the predictor took", end, "seconds")
+                    if not os.path.isdir(md['serialize_path']):
+                        os.makedirs(md['serialize_path'])
+                    predictor.serialize(Path(md['serialize_path'] + "/"))
+                else:
+                    ### Load pre-trained predictors
+                    predictor = fc.load_predictor(md['serialize_path'], md)
 
-                    ### Compute validation metrics
-                    validation_slices = evaluation.split_validation(valid, md)
-                    #validation_slices = validation_slices[:5]
-                    start = time.perf_counter()
-                    forecast = fc.make_forecast_vector(predictor, validation_slices, md)
-                    if md['estimator'] == "TempFlow":
-                        forecast = [Forecast([slice[0].samples[::, ::, n] for n in range(325)], [slice[0].mean[n] for n in range(325)]) for slice in forecast]
-                    else:
-                        forecast = [Forecast([sensor.samples for sensor in slice], [sensor.mean for sensor in slice]) for slice in forecast]
-                    end = time.perf_counter() - start
-                    print("Creating forecasts took", end, "seconds\n Start rescaling")
-                    start = time.perf_counter()
-                    validation_slices, forecast = dp.postprocess_data_vector(validation_slices, forecast)
-                    end = time.perf_counter() - start
-                    print("Rescaling took", end, "seconds\n Start validating...")
-                    start = time.perf_counter()
-                    slices = dp.listdata_to_array(validation_slices)
-                    evals = np.stack(evaluation.validate_mp(slices[1:], forecast[:len(forecast)-1]))
-                    end = time.perf_counter() - start
-                    print("Evaluation took", end, "seconds")
-                    e = np.average(evals)
-                    print(f"Parameter {param} with value {md[param]} had evaluation {e}")
-                    if e < best:
-                        best = e
-                        result = md[param]
-                except:
-                    raise
-            md[param] = result
+                ### Compute validation metrics
+                validation_slices = evaluation.split_validation(valid, md)
+                #validation_slices = validation_slices[:5]
+                start = time.perf_counter()
+                forecast = fc.make_forecast_vector(predictor, validation_slices, md)
+                if md['estimator'] == "TempFlow":
+                    forecast = [Forecast([slice[0].samples[::, ::, n] for n in range(325)], [slice[0].mean[n] for n in range(325)]) for slice in forecast]
+                else:
+                    forecast = [Forecast([sensor.samples for sensor in slice], [sensor.mean for sensor in slice]) for slice in forecast]
+                end = time.perf_counter() - start
+                print("Creating forecasts took", end, "seconds\n Start rescaling")
+                start = time.perf_counter()
+                validation_slices, forecast = dp.postprocess_data_vector(validation_slices, forecast)
+                end = time.perf_counter() - start
+                print("Rescaling took", end, "seconds\n Start validating...")
+                start = time.perf_counter()
+                slices = dp.listdata_to_array(validation_slices)
+                evals = np.stack(evaluation.validate_mp(slices[1:], forecast[:len(forecast)-1]))
+                end = time.perf_counter() - start
+                print("Evaluation took", end, "seconds")
+                e = np.average(evals)
+                print(f"Parameter {param} with value {md[param]} had evaluation {e}")
+                if e < best:
+                    best = e
+                    result = md[param]
+            except:
+                raise
+        md[param] = result
         if not param == 'stop':
             print(f"The final result is {str(md)}\n with evaluation {best}")
             with open(path, "w") as jsonfile:
                 json.dump(md, jsonfile)
-        # with open(md['serialize_path'] + "evaluation.txt", "w") as file:
-        #     e = np.stack(evals)
-        #     for i in range(len(evals)):
-        #         file.write("Slice: " + str(i) + "\n")
-        #         for j in range(len(evals[i])):
-        #             file.write("Sensor: " + str(j) + "\n")
-        #             for k in range(len(evals[i][j])):
-        #                 file.write("{:.2f}".format(evals[i][j][k]) + ", ")
-        #             file.write("\n")
-        #
-        #         file.write("\n\n")
-        #     file.write("Average: " + str(np.average(e)))
+            # with open(md['serialize_path'] + "evaluation.txt", "w") as file:
+            #     e = np.stack(evals)
+            #     for i in range(len(evals)):
+            #         file.write("Slice: " + str(i) + "\n")
+            #         for j in range(len(evals[i])):
+            #             file.write("Sensor: " + str(j) + "\n")
+            #             for k in range(len(evals[i][j])):
+            #                 file.write("{:.2f}".format(evals[i][j][k]) + ", ")
+            #             file.write("\n")
+            #
+            #         file.write("\n\n")
+            #     file.write("Average: " + str(np.average(e)))
     else:
         ### Train network
         if md['train_predictor']:
--- tf-rikke.sh
+++ tf-rikke.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name MySlurmJob # CHANGE this to a name of your choice
 #SBATCH --partition batch # equivalent to PBS batch
 ##SBATCH --time 24:00:00 # Run 24 hours
-#SBATCH --qos=1gpulong # possible values: short, normal, allgpus, 1gpulong
+#SBATCH --qos=allgpus # possible values: short, normal, allgpus, 1gpulong
 #SBATCH --gres=gpu:1 # CHANGE this if you need more or less GPUs
 ##SBATCH --nodelist=nv-ai-01.srv.aau.dk # CHANGE this to nodename of your choice. Currently only two possible nodes are available: nv-ai-01.srv.aau.dk, nv-ai-03.srv.aau.dk
-#SBATCH --array=0-1%2
+#SBATCH --array=4-6
 #srun singularity build --fakeroot mxnet_21.02-py3.sif Singularity
 
-declare -a path=("DF-combine" "DAR-combine" "TF-combine")
+declare -a param=("layers" "cells" "blocks" "hidden_size" "num_hidden" "conditioning" "dropout_rate")
 echo "this is job $SLURM_ARRAY_JOB_ID with task $SLURM_ARRAY_TASK_ID"
-srun singularity exec --nv -B src:/src -B results:/results -B data/"$SLURM_ARRAY_TASK_ID":/data  mxnet_21.02-py3.sif python /src/main.py results/Pems/"${path[$SLURM_ARRAY_TASK_ID]}"/metadata.json
+srun singularity exec --nv -B src:/src -B results:/results -B data/"$SLURM_ARRAY_TASK_ID":/data  mxnet_21.02-py3.sif python /src/main.py results/Pems/TF-combine/metadata.json "${param[$SLURM_ARRAY_TASK_ID]}" "$SLURM_ARRAY_TASK_ID"
 echo "finished successful"
