e6fa7973442c77e675a2a1c6452b0028b4c374b9
AdrianPlesner <adrian.plesner@gmail.com>
2021-05-03 09:15:41 +0200

    update singularity

--- Singularity
+++ Singularity
@@ -6,6 +6,10 @@ pip install tables
 pip install numpy
 pip install pandas
 pip install mxnet
-pip install gluonts
 pip install sklearn
-pip install properscoring
+pip install git+https://github.com/awslabs/gluon-ts.git@master#egg=gluonts
+pip install --no-deps pytorchts==0.3.1
+pip install wandb
+pip install torch
+pip install scipy
+
--- arraytest.sh
+++ arraytest.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1 # CHANGE this if you need more or less GPUs
 ##SBATCH --nodelist=nv-ai-01.srv.aau.dk # CHANGE this to nodename of your choice. Currently only two possible nodes are available: nv-ai-01.srv.aau.dk, nv-ai-03.srv.aau.dk
 #SBATCH --array=0-1%2
-#srun singularity build --fakeroot tensorflow_custom.sif <build-file-path>
+#srun singularity build --fakeroot mxnet_21.02-py3.sif <build-file-path>
 
 declare -a path=("DF-combine" "DAR-combine" "TF-combine")
 echo "this is job $SLURM_ARRAY_JOB_ID with task $SLURM_ARRAY_TASK_ID"
