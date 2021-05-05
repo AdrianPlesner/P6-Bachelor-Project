299d836a2f2307df0e2cb972d21648ed66febe57
AdrianPlesner <adrian.plesner@gmail.com>
2021-05-05 08:44:01 +0200

    fml now fixed 2.1

--- tf-rikke.sh
+++ tf-rikke.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=allgpus # possible values: short, normal, allgpus, 1gpulong
 #SBATCH --gres=gpu:1 # CHANGE this if you need more or less GPUs
 ##SBATCH --nodelist=nv-ai-01.srv.aau.dk # CHANGE this to nodename of your choice. Currently only two possible nodes are available: nv-ai-01.srv.aau.dk, nv-ai-03.srv.aau.dk
-#SBATCH --array=4-6
+#SBATCH --array=4,6
 #srun singularity build --fakeroot mxnet_21.02-py3.sif Singularity
 
 declare -a param=("layers" "cells" "blocks" "hidden_size" "num_hidden" "conditioning" "dropout_rate")
