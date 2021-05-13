ac7ad6fd2c0d491634c3be95ea394bce2bf7778c
AdrianPlesner <adrian.plesner@gmail.com>
2021-05-13 10:04:45 +0200

    Done some stuff

--- src/make_forecast.py
+++ src/make_forecast.py
@@ -27,7 +27,7 @@ def train_predictor(data=None, md=None):
         exit("Missing metadata for training")
     if data is None:
         exit("Missing data for training")
-    trainer = Trainer(ctx=mx.context.cpu(),
+    trainer = Trainer(ctx=mx.context.gpu(),
                       epochs=50,
                       batch_size=32,
                       learning_rate=1e-3,
@@ -106,7 +106,7 @@ def train_predictor(data=None, md=None):
             num_cells=md['cells'],
             cell_type=md['cell_type'],
             dropout_rate=md['dropout_rate'],
-            use_feat_static_real=True,
+            use_feat_static_real=False,
             distr_output=distribution
         )
     elif md['estimator'] == "TempFlow":
