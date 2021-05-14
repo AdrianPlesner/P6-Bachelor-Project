b19d83c0b493b421d2c613aa62d035f39b26a35b
AdrianPlesner <adrian.plesner@gmail.com>
2021-05-14 14:46:13 +0200

    fix baseline

--- src/baseline.py
+++ src/baseline.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         sensors.append(days)
     print("made object")
     for s in range(len(train.list_data)):
-        i = 3
+        i = 6
         j = 0
         k = 0
         sens = train.list_data[s]
@@ -76,7 +76,19 @@ if __name__ == '__main__':
                         i = 0
     print("done looking")
     test_slices = ev.split_validation(test, md)
-    slices = listdata_to_array(test_slices)
-    forecasts = [make_forecast(slice) for slice in test_slices]
-    mse = np.stack(ev.validate_mp(slices, forecasts, mse=True))
-    crps = np.stack(ev.validate_mp(slices, forecasts, mse=False))
+    data_slices = listdata_to_array(test_slices)
+    ss = []
+    while len(test_slices) > 100:
+        ss.append((test_slices[:100],data_slices[:100]))
+        test_slices = test_slices[100:]
+        data_slices = data_slices[100:]
+    ss.append((test_slices, data_slices))
+    test_slices = None
+    data_slices = None
+    mse = []
+    crps = []
+    for slices in ss:
+        forecasts = [make_forecast(slice) for slice in slices[0]]
+        mse.append(np.average(np.stack(ev.validate_mp(slices[1], forecasts, mse=True))))
+        crps.append(np.average(np.stack(ev.validate_mp(slices[1], forecasts, mse=False))))
+    print(f'crps: {np.average(np.stack(crps))}, mse: {np.average(np.stack(mse))}')
